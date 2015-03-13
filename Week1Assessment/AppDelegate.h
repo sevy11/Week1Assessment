f0e1ccb09913f8d1d433897ce8fbccfc0887887a
Sevy11 <michaelsevy@gmail.com>
2015-03-13 09:07:09 -0500

    Initial Commit

---
+++ Week1Assessment/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Week1Assessment
+//
+//  Created by Michael Sevy on 3/13/15.
+//  Copyright (c) 2015 Michael Sevy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Week1Assessment/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Week1Assessment
+//
+//  Created by Michael Sevy on 3/13/15.
+//  Copyright (c) 2015 Michael Sevy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
